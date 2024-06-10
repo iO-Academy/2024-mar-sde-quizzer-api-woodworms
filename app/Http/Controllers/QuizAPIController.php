f3f97a5684cb47f0d9beb22a8be208ce8085c89a
Louis <louisprotano@hotmail.com>
2024-06-10 14:50:00 +0100

    fixed .env being in .gitignore

--- app/Http/Controllers/QuizAPIController.php
+++ app/Http/Controllers/QuizAPIController.php
@@ -4,15 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\Quiz;
 use Illuminate\Http\Request;
+use function PHPUnit\Framework\isEmpty;
 
 class QuizAPIController extends Controller
 {
     function getQuizzes()
     {
-        $quizzes = Quiz::all();
-        return response()->json([
-            'message' => 'Quizzes retrieved',
-            'data' => $quizzes
-        ], 200);
+//        if (class_exists('Quiz')) {
+            $quizzes = Quiz::all();
+
+            if ($quizzes->isEmpty()) {
+                return response()->json([
+                    "message" => "No quizzes found"
+                ]);
+            }
+            return response()->json([
+                'message' => 'Quizzes retrieved',
+                'data' => $quizzes
+            ], 200);
+//        }
+//        else {
+//            return response()->json([
+//                'message' => 'Quizzes not found, please check database connection',
+//            ], 500);
+//        }
     }
 }
