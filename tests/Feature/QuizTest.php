36f307e28a8e9030794797fdf2f9baf797744905
John <johnlouisstephenson@gmail.com>
2024-06-11 09:59:34 +0100

    Updated tests for creating quizzes

--- tests/Feature/QuizTest.php
+++ tests/Feature/QuizTest.php
@@ -27,5 +27,73 @@ class QuizTest extends TestCase
         });
     }
 
+    public function test_createTodo_success(): void
+    {
+        $testData = ['name' => 'Test task', 'description' => 'Test, test'];
+
+        $response = $this->postJson('/api/quizzes', $testData);
+        $response->assertStatus(201)
+            ->assertJson(function(AssertableJson $json) {
+                $json->hasAll(['message']);
+            });
+
+        $this->assertDatabaseHas('quizzes', $testData);
+    }
+
+    public function test_createTodo_failureNameRequired(): void
+    {
+        $testData = ['description' => 'Test, test'];
+
+        $response = $this->postJson('/api/quizzes', $testData);
+        $response->assertStatus(422)
+            ->assertJson(function(AssertableJson $json) {
+                $json->hasAll(['message', 'errors'])
+                    ->has('errors', function (AssertableJson $json) {
+                        $json->hasAll( 'name');
+            });
+        });
+    }
+
+    public function test_createTodo_failureDescriptionRequired(): void
+    {
+        $testData = ['name' => 'Test'];
+
+        $response = $this->postJson('/api/quizzes', $testData);
+        $response->assertStatus(422)
+            ->assertJson(function(AssertableJson $json) {
+                $json->hasAll(['message', 'errors'])
+                    ->has('errors', function (AssertableJson $json) {
+                        $json->hasAll( 'description');
+                    });
+            });
+    }
+
+    public function test_createTodo_malformedName(): void
+    {
+        $testData = ['name' => 10, 'description' => 'Test'];
+
+        $response = $this->postJson('/api/quizzes', $testData);
+        $response->assertStatus(422)
+            ->assertJson(function(AssertableJson $json) {
+                $json->hasAll(['message', 'errors'])
+                    ->has('errors', function (AssertableJson $json) {
+                        $json->hasAll( 'name');
+                    });
+            });
+    }
+
+    public function test_createTodo_malformedDescription(): void
+    {
+        $testData = ['name' => 'Test', 'description' => 10];
+
+        $response = $this->postJson('/api/quizzes', $testData);
+        $response->assertStatus(422)
+            ->assertJson(function(AssertableJson $json) {
+                $json->hasAll(['message', 'errors'])
+                    ->has('errors', function (AssertableJson $json) {
+                        $json->hasAll( 'description');
+                    });
+            });
+    }
 
 }
