d1ea93939f587d811976e0c8724a5e03a470da3e
John <johnlouisstephenson@gmail.com>
2024-06-13 11:54:30 +0100

    Pseudocode for service

---
+++ app/Services/CalculateScoreService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+class CalculateScoreService
+{
+    // $questionCount = 0
+    // $availablePoints = 0
+    // $points = 0
+    // $answers = $request->answers
+    // $quizId = $request->id
+    // foreach ($answers as $answer)
+    // $currentAnswer = Answer::find($answer->answer)
+
+
+    // $currentQuestion = Question::find($answer->question)
+
+    // $questionCount++
+    // how many points it's worth - $questionPoints = $currentQuestion->points
+    // $availablePoints += $questionPoints
+    // if correct
+    // $points += $questionPoints
+    // $correctCount++
+
+    // return array ['question_count' => $questionCount, 'correct_count' => $correctCount, 'available_points' => $availablePoints
+    // , 'points' => $points]
+
+    // If any answers incorrect = 0 points,
+    // If only correct answers selected = full marks
+}
